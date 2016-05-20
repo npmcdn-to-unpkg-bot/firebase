499919c51764022daaccfe973e6542dad49c5dc8
Geoff McCarron-Benson <geoff@geofftech.com>
2016-05-21 00:39:36 +1000

    Add Firebase list to HomeList

--- router/homelist.js
+++ router/homelist.js
@@ -1,16 +1,149 @@
 import React from 'react';
 import { Link } from 'react-router';
 import NavBar from './navbar.js';
+// import Firebase from 'reactfire';
 
-class HomeList extends React.Component {
+const firebaseDatabase = 'testing-2f687';
+const firebasetable = 'location';
+const firebaseURL = 'https://'+firebaseDatabase+'.firebaseio.com/'+firebasetable;
+
+class HomeListItem extends React.Component {
   render() {
     return (
+      <tr key={ this.props.item.handle }>
+        <td>
+          { this.props.item.handle }
+        </td>
+        <td>
+          { this.props.item.title }
+        </td>
+        <td>
+          { this.props.item.phone }
+        </td>
+        <td>
+          <button onClick={this.props.removeItemFn.bind(null, this.props.item.handle)}>
+            Remove
+          </button>
+        </td>
+      </tr>
+    )
+  }
+}
+
+class HomeListTable extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      search: ''
+    };
+  }
+  updateSearch(event) {
+    this.setState({
+      search: event.target.value
+    });
+  }
+  render() {
+    let filteredItems = this.props.items.filter(
+      (item) => {
+        return item.title.toLowerCase().indexOf(
+          this.state.search.toLowerCase()
+        ) !== -1;
+      }
+    );
+    return (
       <div>
-        <h1>Home</h1>
-        <NavBar />
+        <div className="form-group">
+          <label className="col-xs-2 control-label">Search</label>
+          <div className="col-xs-10">
+          <input type="text"
+            value={this.state.search}
+            onChange={this.updateSearch.bind(this)}
+          />
+          </div>
+        </div>
+        <table className="table table-border">
+          <thead>
+            <tr>
+              <th>Handle</th>
+              <th>Title</th>
+              <th>Phone</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredItems.map(
+              (item) => {
+                return (
+                  <HomeListItem
+                    item={item}
+                    key={item.handle}
+                    removeItemFn={this.props.removeItemFn} />
+                );
+              }
+            )}
+          </tbody>
+        </table>
       </div>
     )
   }
 }
 
+class HomeList extends React.Component {
+  constructor() {
+    super();
+    this.firebaseRef = new Firebase(firebaseURL);
+    this.state = {
+      items: [],
+    };
+  }
+  componentWillMount() {
+    this.firebaseRef.on('value', this.addItemsFromResponse.bind(this));
+  }
+  componentWillUnmount() {
+    this.firebaseRef.off();
+  }
+  addItemsFromResponse(dataSnapshot) {
+    var items = [];
+    dataSnapshot.forEach(function(childSnapshot) {
+      var item = childSnapshot.val();
+      item.handle = childSnapshot.key();
+      items.push(item);
+    }.bind(this));
+    this.setState({
+      items: items
+    });
+  }
+  addItem(handle, values) {
+    console.log('addItem', handle, values);
+    if (!handle || handle.trim().length == 0) {
+      console.error('addItem', 'Invalid Handle', handle);
+      return 'Invalid Handle Length';
+    }
+    if (!values.title || values.title.trim().length == 0) {
+      console.error('addItem', 'Invalid Title', values);
+      return 'Invalid Title';
+    }
+    let item = {};
+    item.handle = values.handle;
+    item.title = values.title;
+    item.phone = values.phone;
+    console.log('addItem', 'item', item);
+    // define the id to use
+    let send = {};
+    send[handle] = item;
+    console.log('addItem', 'send', send);
+    this.firebaseRef.update(send);
+  }
+  removeItem(handle) {
+    let firebaseRef = new Firebase(firebaseURL);
+    firebaseRef.child(handle).remove();
+  }
+  render() {
+    return (
+      <div>
+        <HomeListTable items={this.state.items} removeItemFn={this.removeItem} />
+      </div>
+    )
+  }
+}
 export default HomeList;
--- router/index.md
+++ router/index.md
@@ -4,6 +4,8 @@ layout: default
 xjavascript:
 - https://npmcdn.com/react-router@2.4.0
 javascript:
+- https://cdn.firebase.com/js/client/2.4.2/firebase.js
+- https://cdn.firebase.com/libs/reactfire/0.7.0/reactfire.js
 - router.js
 ---
 
